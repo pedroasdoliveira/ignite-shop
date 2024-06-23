aeceeff8b0f23fac81e4659097797d872fb10a95
Pedro Oliveira <Pedroaolive@gmail.com>
2024-06-23 18:27:08 -0300

    feat: creating success page while checkout is success

--- src/pages/api/checkout/index.ts
+++ src/pages/api/checkout/index.ts
@@ -17,7 +17,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).json({ error: "Product price not found!" });
     }
 
-    const successUrl = `${process.env.NEXT_URL}/success`;
+    const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
     const cancelUrl = `${process.env.NEXT_URL}/`;
 
     const checkoutSession = await stripe.checkout.sessions.create({
---
+++ src/pages/success/index.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { SuccessContainer, ImageContainer } from "../../styles/pages/success";
+import { GetServerSideProps } from "next";
+import { stripe } from "../../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
+
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+const Success = ({ customerName, product }: SuccessProps) => {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="Produto" />
+      </ImageContainer>
+
+      <p>
+        <strong>{customerName}</strong>, sua <strong>{product.name}</strong> foi
+        efetuada com sucesso e logo chegará a você.
+      </p>
+
+      <Link href={"/"}>Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
+};
+
+export default Success;
+
+export const getServerSideProps: GetServerSideProps<SuccessProps> = async ({
+  query,
+}) => {
+  const { session_id } = query;
+  const sessionId = String(session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
